f92812712e19aac3cc3d586e8b9b731f96aad1f7
Varshith Kumar Soma <22311a6904@sreenidhi.edu.in>
2024-07-07 19:51:34 +0530

    Add files via upload

---
+++ 29.asm
@@ -0,0 +1,19 @@
+assume cs:code
+assume es:extra
+extra segment
+str1 db 'micro',0
+count equ 0ffh
+extra ends
+code segment
+start:
+mov ax,extra
+mov es,ax
+lea di,str1
+mov cl,count
+cld
+mov al,0
+repne scasb
+dec di
+int 03h
+code ends
+end start
