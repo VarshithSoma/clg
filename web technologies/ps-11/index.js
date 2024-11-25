dc074e71cded50dfae67eceed4d8cb11b8585827
Varshith Kumar Soma <varshithkumarsoma@gmail.com>
2024-11-26 00:20:40 +0530

    added some codes

--- web technologies/ps-11/index.js
+++ web technologies/ps-11/index.js
@@ -11,3 +11,28 @@ app.use(
     },
   })
 );
+app.get("/", (req, res) => {
+  res.send("welcome session exaple");
+});
+app.get("/login", (req, res) => {
+  req.session.user = { username: "varshith" };
+  res.send("user logged in");
+});
+app.get("/profile", (req, res) => {
+  if (req.session.user) {
+    res.send(`user  logged in ${req.session.user.username}`);
+  } else {
+    return res.send("user not logged in");
+  }
+});
+app.get("/logout", (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      return res.send("user not logged in");
+    }
+    res.send("user  logged out success");
+  });
+});
+app.listen(8056, () => {
+  console.log("eke");
+});
