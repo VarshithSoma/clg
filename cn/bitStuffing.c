68460ec3faa4e26647d7dcc0dbe436c1525920bf
Varshith Kumar Soma <varshithkumarsoma@gmail.com>
2024-12-18 12:30:55 +0530

    dijkstra

--- cn/bitStuffing.c
+++ cn/bitStuffing.c
@@ -1,32 +1,31 @@
 #include<stdio.h>
-#include<string.h>
 int main(){
     int n;
-    int j=0;
-    int a[20];
-    int b[20];
-    int count=0;
-    printf("Enter the Frame Length: ");
+    printf("Enter the Number of bits 0s and 1s: ");
     scanf("%d",&n);
-    printf("Enter the binary frame 0s and 1s: \n");
+    int data[10];
+    printf("Enter the data bits : ");
     for(int i=0;i<n;i++){
-        scanf("%d",&a[i]);
+        scanf("%d",&data[i]);
     }
+    int count=0;
+    int output[10];
+    int j=0;
     for(int i=0;i<n;i++){
-        b[j++]=a[i];
-        if(a[i]==1){
+        if(data[i]==1){
             count++;
+            output[j++]=data[i];
             if(count==5){
-                b[j++]=0;
+                output[j++]=0;
                 count=0;
             }
         }else{
+            output[j++]=data[i];
             count=0;
         }
     }
-    printf("\n After Character Stuffing The Frame is : \n");
+    printf("The data Frame is :\n");
     for(int i=0;i<j;i++){
-        printf("%d ",b[i]);
+        printf("%d ",output[i]);
     }
-    printf("\n");
 }
--- cn/hammingCode.c
+++ cn/hammingCode.c
@@ -1,45 +1,32 @@
-#include <stdio.h>
-
-void main() {
-    int data[7], rec[7], i, c1, c2, c3, error_position;
-
-    printf("This works for a message of 4 bits.\nEnter the message bits one by one: ");
-    scanf("%d %d %d %d", &data[0], &data[1], &data[2], &data[4]);
-
-    // Calculate parity bits
-    data[6] = data[0] ^ data[2] ^ data[4]; // P1
-    data[5] = data[0] ^ data[1] ^ data[4]; // P2
-    data[3] = data[0] ^ data[1] ^ data[2]; // P4
-
-    printf("\nThe encoded bits are: ");
-    for (i = 0; i < 7; i++) {
-        printf("%d ", data[i]);
+#include<stdio.h>
+int main(){
+    int data[20],rec[20];
+    printf("Enter 4 bit data one by one : \n");
+    scanf("%d %d %d %d",&data[0],&data[1],&data[2],&data[4]);
+    data[6]=data[0]^data[2]^data[4];
+    data[5]=data[4]^data[1]^data[0];
+    data[3]=data[0]^data[1]^data[2];
+    printf("The sending encoded bits are : ");
+    for(int i=0;i<7;i++){
+        printf("%d ",data[i]);
     }
-
-    printf("\n\nEnter the received data bits one by one: ");
-    for (i = 0; i < 7; i++) {
-        scanf("%d", &rec[i]);
+    printf("\nEnter the received bits : \n");
+    for(int i=0;i<7;i++){
+        scanf("%d",&rec[i]);
     }
-
-    // Check parity bits for error detection
-    c1 = rec[6] ^ rec[4] ^ rec[2] ^ rec[0]; // P1 check
-    c2 = rec[5] ^ rec[4] ^ rec[1] ^ rec[0]; // P2 check
-    c3 = rec[3] ^ rec[2] ^ rec[1] ^ rec[0]; // P4 check
-
-    error_position = c3 * 4 + c2 * 2 + c1; // Determine error position
-
-    if (error_position == 0) {
-        printf("\nNo error detected. The received message is correct.\n");
-    } else {
-        printf("\nError detected at position: %d\n", error_position);
-
-        // Correct the error
-        rec[7 - error_position] = rec[7 - error_position] ^ 1;
-
-        printf("The corrected message is: ");
-        for (i = 0; i < 7; i++) {
-            printf("%d ", rec[i]);
+    int c1=rec[6]^rec[0]^rec[2]^rec[4];
+    int c2=rec[4]^rec[1]^rec[0]^rec[5];
+    int c3=rec[3]^rec[0]^rec[1]^rec[2];
+    int del=(c3*4)+(c2*2)+(c1);
+    if(del==0){
+        printf("no error in data recived");
+    }else{
+        printf("error at position %d , %d",del,rec[7-del]);
+        rec[7-del]=rec[7-del]^1;
+          printf("%d",rec[7-del]);
+        printf("\nthe correct data is \n");
+        for(int i=0;i<7;i++){
+            printf("%d ",rec[i]);
         }
-        printf("\n");
     }
 }
