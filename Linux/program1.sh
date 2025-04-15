1ccc87a4d51f0941047cf6c038c63ea86fe59e74
varshithkumarsoma <varshithkumarsoma@gmail.com>
2025-04-15 20:15:46 +0530

    feat: add scripts

--- Linux/program1.sh
+++ Linux/program1.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
-echo "enter file name:"
-read file
-echo "enter starting postion"
-read starting
-echo "enter ending position"
-read ending
-echo "the lines between $starting and $ending from $file"
-if [ $starting -lt $ending ]; then
-  n1=$(expr $starting + 1)
-  n2=$(expr $ending - 1)
-  head -$n2 $file | tail +$n1
-elif [ $starting -gt $ending ]; then
-  n3=$(expr $starting - 1)
-  n4=$(expr $ending + 1)
-  head -$n3 $file | tail +$n4
+
+echo "Enter file name"
+read f
+echo "Enter the starting position"
+read s
+echo "Enter the ending postion"
+read e
+if [ $s -lt $e ]
+then
+n1=` expr $s + 1 `
+n2=` expr $e - 1 `
+head -$n2 $f|tail -$n1
+else
+n1=` expr $e + 1 `
+n2=` expr $s - 1 `
+head -$n2 $f|tail -$n1
 fi
+
